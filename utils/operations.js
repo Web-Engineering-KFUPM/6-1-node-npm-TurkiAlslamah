77eec288a35e0c3bfa2a2a91bd80723bd32b3b92
TurkiAlslamah <s202172030@kfupm.edu.sa>
2025-11-11 20:03:02 +0300

    todo

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,22 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((res, num) => res - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((res, num) => res * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((res, num) => {
+    if (num === 0) {
+      console.log("Error: Division by zero");
+      process.exit(1);
+    }
+    return res / num;
+  }, numbers[0]);
 }
 
